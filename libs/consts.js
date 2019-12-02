63f802c06d869e810d23acdb80928f8d674fbb43
夏稳 <xiawen@xiawendeMacBook-Pro.local>
2019-12-02 11:21:45 +0800

    feat: w

--- libs/consts.js
+++ libs/consts.js
@@ -2,8 +2,9 @@
  * @Author:xiawen
  * @Date:2019-11-29 18:31:05
  * @LastModifiedBy:xiawen
- * @Last Modified time:2019-11-29 18:31:05
+ * @Last Modified time:2019-11-29 22:56:57
  */
+
 
 module.exports.templateUrl = {
   gitIgnore: 'https://raw.githubusercontent.com/xiaotangdou/lazy-ac-cli/master/templates/gitignore.tpl'
@@ -11,4 +12,14 @@ module.exports.templateUrl = {
 
 module.exports.weatherUrl = {
   baidu: 'https://api.seniverse.com/v3/'
+}
+
+module.exports.inquirerMsg = {
+  confirm: {
+    override: {
+      type: 'confirm',
+      name: 'override',
+      message: '是否覆盖？'
+    }
+  }
 }
